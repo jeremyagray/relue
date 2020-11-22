68005a3e83910c17f71cb9ec336db8c05f1048bd
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-21 19:48:00 -0600

    feat: add documentation for all functions
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-module.exports = require('./lib/relue/index.js');
+/**
+ * @fileOverview Top level module for relue.
+ *
+ * @author Jeremy A Gray <jeremy.a.gray@gmail.com>
+ *
+ * relue
+ *
+ * @module relue
+ * @namespace relue
+ */
+
+exports.math = require('./lib/relue/math/index.js');
+exports.object = require('./lib/relue/object/index.js');
+exports.text = require('./lib/relue/text/index.js');
